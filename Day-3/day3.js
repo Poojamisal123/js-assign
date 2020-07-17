798d2741b72e1004a6fdbc9ad5879b7ca56b88f0
Poojamisal123 <60282803+Poojamisal123@users.noreply.github.com>
2020-07-17 16:36:52 +0530

    Assignment-3

---
+++ Day-3/day3.js
@@ -0,0 +1,13 @@
+
+function check() {
+    var v = document.getElementById('evenoddinput').value;
+
+ if (v % 2 == 0) {
+      console.log("The number entered is " + v + " and number is even");
+ }
+ else{
+    console.log("The number entered is "  + v + " and number is odd");
+ }
+    
+  }
+
---
+++ Day-3/day3q2.js
@@ -0,0 +1,4 @@
+let str = 'Android' 
+let strnum ='9'
+console.log(`The OS name is  ${str} and version is ${strnum}`);
+
